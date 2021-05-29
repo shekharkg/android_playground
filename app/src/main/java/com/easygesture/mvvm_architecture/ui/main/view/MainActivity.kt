abffc1900e0a82ada19db7f57e7c54e738f71d05
shekhar.gupta <shekhar.gupta@eagleview.com>
2021-05-29 17:45:27 +0530

    feat: setup main view model

--- app/src/main/java/com/easygesture/mvvm_architecture/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/easygesture/mvvm_architecture/ui/main/view/MainActivity.kt
@@ -1,9 +1,13 @@
-package com.easygesture.mvvm_architecture
+package com.easygesture.mvvm_architecture.ui.main.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.easygesture.mvvm_architecture.R
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
